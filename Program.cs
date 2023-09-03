189d812c127cd7c1164338ba7d87fc29b5f26cfe
ashu1-12 <sadanshivashish122@gmail.com>
2023-09-03 20:25:58 +0530

    Perfect Number

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            FibonacciSeries fibonacciSeries = new FibonacciSeries();
+            //FibonacciSeries fibonacciSeries = new FibonacciSeries();
             int n= Convert.ToInt32(Console.ReadLine());
             //fibonacciSeries.fibonacciSeries(n);
             PerfectNumber perfectNumber = new PerfectNumber(n);
