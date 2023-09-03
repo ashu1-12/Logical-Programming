f8e4d2822dc3bb1433fab9ef53a7d44b4f9b6f79
ashu1-12 <sadanshivashish122@gmail.com>
2023-09-03 21:10:16 +0530

    Reverse Number

--- Program.cs
+++ Program.cs
@@ -8,10 +8,13 @@
             //FibonacciSeries fibonacciSeries = new FibonacciSeries();
             int n= Convert.ToInt32(Console.ReadLine());
             //fibonacciSeries.fibonacciSeries(n);
-           // PerfectNumber perfectNumber = new PerfectNumber(n);
-            PrimeNumber primeNumber = new PrimeNumber();
-            bool flag = primeNumber.IsPrimeNumber(n);
-            Console.WriteLine(n + " Is prime Number "+flag);
+            // PerfectNumber perfectNumber = new PerfectNumber(n);
+            //PrimeNumber primeNumber = new PrimeNumber();
+            //bool flag = primeNumber.IsPrimeNumber(n);
+            //Console.WriteLine(n + " Is prime Number "+flag);
+            ReverseNumber reverseNumber = new ReverseNumber();
+            int reversedVal = reverseNumber.reversedNumber(n);
+            Console.WriteLine("Reversing the number " + n + " is " + reversedVal);
         }
     }
 }
---
+++ ReverseNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class ReverseNumber
+    {
+        public int  reversedNumber(int num) {
+           int reverse = 0;
+           while (num > 0)
+            {
+                int rem = num % 10;
+                num /= 10;
+                reverse = reverse*10 + rem;    
+            }
+           return reverse;
+        }
+    }
+}
